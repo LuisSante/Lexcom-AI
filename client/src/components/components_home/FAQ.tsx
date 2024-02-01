776ab84d13dc1b4cc211a36d5c580a7daa330b76
LuisSante <lsantet@unsa.edu.pe>
2024-02-01 14:45:27 -0500

    feat: styles accordion

--- client/src/components/components_home/FAQ.tsx
+++ client/src/components/components_home/FAQ.tsx
@@ -1,22 +1,12 @@
 import '../../css/styles_home/faq.css';
-import { Collapse } from 'antd';
+import { Collapse, ConfigProvider } from 'antd';
 import FAQItems from '../logic/FAQItems';
+import React from "react";
+
+
 interface ComponentProps {
   id: string;
 }
-
-const customPanelStyle = {
-  background: '#0561ac',
-  borderRadius: 0,
-  border: 1,
-  overflow: 'hidden',
-  color: 'rgb(239 68 68)'
-};
-
-const customCollapseStyle = {
-  borderRadius: 4,
-  borderColor: '#0561ac',
-};
 
 
 const Faq: React.FC<ComponentProps> = ({ id }) => {
@@ -30,13 +20,37 @@ const Faq: React.FC<ComponentProps> = ({ id }) => {
         <span style={{ color: '#0561ac' }}>frecuentes </span>
         <span style={{}}>preguntas</span>
       </div>
-      <Collapse style={customCollapseStyle} accordion>
+      <ConfigProvider
+      theme={{
+        components: {
+          Collapse: {
+            colorTextHeading: "rgba(255, 255, 255, 1)", // question
+            colorBgContainer: "rgba(58, 64, 75, 0.92)", // answer
+            headerBg: "rgba(58, 64, 75, 0.92)",
+            colorText: "rgba(255, 255, 255, 1)",
+            colorBorder: "rgba(255, 255, 255, 1)",
+            lineHeight: 1.5,
+            motionDurationMid: "0.5s",
+          },
+        },
+        token: {
+          fontFamily: "Poppins, sans-serif",
+          padding: 20,
+          motionEaseInOut: "cubic-bezier(1, 1, 1, 1)",
+          marginSM: 20,
+          fontSizeIcon: 20,
+          fontSize: 20,
+        },
+      }}
+    >
+      <Collapse accordion>
         {FAQItems.map((item) => (
-          <Panel header={item.question} key={item.key} style={customPanelStyle}>
-            <span className='answer'>{item.answer}</span>
+          <Panel header={item.question} key={item.key}>
+            {item.answer}
           </Panel>
         ))}
       </Collapse>
+    </ConfigProvider>
     </div>
   )
 }
