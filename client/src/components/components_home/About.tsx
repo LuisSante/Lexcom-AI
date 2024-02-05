3f7372adcf601288da1f422dd93a3ffe9d0c2796
LuisSante <lsantet@unsa.edu.pe>
2024-02-04 22:20:26 -0500

    feat: prime react

--- client/src/components/components_home/About.tsx
+++ client/src/components/components_home/About.tsx
@@ -5,10 +5,13 @@ interface ComponentProps {
   id: string
 }
 
-const About: React.FC<ComponentProps> = ({id}) =>{
+const About: React.FC<ComponentProps> = ({ id }) => {
   return (
-    <div id = {id}>
-      <Footer style={{background: 'black', color: 'white', textAlign: 'center' }}>
+    <div id={id}>
+      <Footer style={{ background: 'black', color: 'white', textAlign: 'center' }}>
+        <a href='mailto:lsantet@unsa.edu.pe'> Legal </a>
+        <a href='mailto:lsantet@unsa.edu.pe'> Support </a>
+        <br />
         Lexcom ©{new Date().getFullYear()} Created by Lexcom
       </Footer>
     </div>
