bd0e504c808cd97b0b97258cbbcb06ea96906937
LuisSante <lsantet@unsa.edu.pe>
2024-02-22 19:36:39 -0500

    feat: skeleton for load in component product

--- client/src/pages/Product.tsx
+++ client/src/pages/Product.tsx
@@ -1,12 +1,28 @@
+import { useEffect, useState } from "react";
 import Region from "../components/components_product/Region";
 import Topics from "../components/components_product/Topics";
 import Trends from "../components/components_product/Trends";
+import Skeleton from "../components/Skeleton";
 
 interface TypeProduct {
   searchValue: string;
 }
 
 const Product: React.FC<TypeProduct> = ({ searchValue }) => {
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  if (loading) {
+    return <Skeleton/>;
+  }
   
   return (
     <>
