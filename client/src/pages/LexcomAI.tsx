0650552ba32500ad5076f9ff77ff19ccfbf0b9c4
LuisSante <lsantet@unsa.edu.pe>
2024-02-24 10:55:36 -0500

    feat: new changes

--- client/src/pages/LexcomAI.tsx
+++ client/src/pages/LexcomAI.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import LexcomAI_Items from '../components/logic/LexcomAI_Items';
-import { Checkbox, ConfigProvider, Form } from 'antd';
+import { Checkbox, Col, ConfigProvider, Form, Row, Space, Typography } from 'antd';
 import { Input } from 'antd';
-
+import './../css/TimelineDemo.css';
 
 const onFinish = () => {
     console.log('asdf')
@@ -10,49 +10,55 @@ const onFinish = () => {
 
 const LexcomAI: React.FC = () => {
     return (
-        <ConfigProvider
-            theme={{
-                token: {
-                    colorText: "#fff",
-                }
-            }}
-        >
-            <Form
-                name="basic"
-                labelCol={{ span: 8 }}
-                wrapperCol={{ span: 16 }}
-                style={{ maxWidth: '100%' }}
-                initialValues={{ remember: true }}
-                onFinish={onFinish}
-                autoComplete="off"
+        <div className='tutorial'>
+             <Typography.Title level={4} style={{ color: '#fff' }}>¡Vamos a calcular si tu producto es ganador!</Typography.Title>
+
+            <ConfigProvider
+                theme={{
+                    token: {
+                        colorText: "#fff",
+                    }
+                }}
             >
-                <Form.Item
-                    label="Cuánto es el interés que hay sobre el producto"
-                    name="interest"
-                    rules={[{ required: true, message: 'Please input your username!' }]}
+                <Form
+                    name="basic"
+                    labelCol={{ span: 20 }}
+                    wrapperCol={{ span: 8 }}
+                    style={{ maxWidth: '100%' }}
+                    initialValues={{ remember: true }}
+                    onFinish={onFinish}
+                    autoComplete="off"
                 >
-                    <br />
-                    <Input />
-                </Form.Item>
-
-
-                {LexcomAI_Items.map((item, index) => (
                     <Form.Item
-                        name={item.id}
-                        valuePropName="checked"
-                        wrapperCol={{ offset: 8, span: 16 }}
+                        label="Cuánto es el interés que hay sobre el producto"
+                        name="interest"
                         rules={[{ required: true, message: 'Please input your username!' }]}
                     >
-                        <Checkbox key={index}>
-                            {item.question}
-                        </Checkbox>
+                        <br />
+                        <Input />
                     </Form.Item>
-                ))}
+                    <Row>
+                    {LexcomAI_Items.map((item, index) => (
+                        <Form.Item
+                            name={item.id}
+                            valuePropName="checked"
+                            wrapperCol={{ offset: 5, span: 25 }}
+                            rules={[{ required: true, message: 'Please input your username!' }]}
+                        >
+                          <Col span={15}>
+                            <Checkbox key={index}>
+                                {item.question}
+                            </Checkbox>
+                          </Col>
+                        </Form.Item>
+                    ))}    
+                    </Row>
 
 
-            </Form>
+                </Form>
 
-        </ConfigProvider>
+            </ConfigProvider>
+        </div>
     )
 }
 
