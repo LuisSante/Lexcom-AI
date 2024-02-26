ea2d93bf3656da6a8d3e81f311aaab44f44c3a5a
MaryoriHilares <mhilaresa@unsa.edu.pe>
2024-02-26 17:06:20 -0500

    map clorophet

--- client/src/components/components_product/Region.tsx
+++ client/src/components/components_product/Region.tsx
@@ -1,6 +1,21 @@
 import axios, { AxiosResponse } from "axios";
 import { useEffect, useState } from "react";
 import { Select } from 'antd';
+import { scaleLinear } from "d3-scale";
+import {
+  ComposableMap,
+  Geographies,
+  Geography,
+  Sphere,
+  Graticule
+} from "react-simple-maps";
+
+
+const geoUrl: string = "/features.json";
+
+const colorScale = scaleLinear<string>()
+  .domain([0.29, 0.68])
+  .range(["#ffedea", "#ff5233"]);
 
 interface RegionData {
   geo: string;
@@ -15,6 +30,7 @@ interface TypeRegion {
 const Region: React.FC<TypeRegion> = ({ searchValue }) => {
   const [data, setData] = useState<RegionData[] | null>(null);
 
+  const [dataR, setDataR] = useState<RegionData[] | null>(null);
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -31,6 +47,17 @@ const Region: React.FC<TypeRegion> = ({ searchValue }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [searchValue]);
 
+  useEffect(() => {
+    if (data) {
+      const maximoValor = Math.max(...data.map(item => item.value));
+      const dataActualizado = data.map(item => ({
+        ...item,
+        value: item.value / maximoValor
+      }));
+      setDataR(dataActualizado);
+    }
+  }, [data]);
+
   const onChange = (value: string) => {
     console.log(`selected ${value}`);
   };
@@ -46,8 +73,36 @@ const Region: React.FC<TypeRegion> = ({ searchValue }) => {
 
   return (
     <div>
-      {data && (
-        <div>
+      {data &&dataR && (
+        <div>   
+          <ComposableMap
+            projectionConfig={{
+              rotate: [-10, 0, 0],
+              scale: 130
+            }}
+            style={{
+              marginTop: "-100px" 
+            }}
+          >
+            <Sphere stroke="#E4E5E6" strokeWidth={0.5} id={""} fill={""} />
+            <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
+            {dataR.length > 0 && (
+              <Geographies geography={geoUrl}>
+                {({ geographies }) =>
+                  geographies.map((geo) => {
+                    const d = dataR.find((s) => s.location === geo.properties.name);
+                    return (
+                      <Geography
+                        key={geo.rsmKey}
+                        geography={geo}
+                        fill={d ? colorScale(d.value ) : "#F5F4F6"}
+                      />
+                    );
+                  })
+                }
+              </Geographies>
+            )}
+          </ComposableMap>
           <Select
             showSearch
             placeholder="Selecciona un país"
@@ -63,7 +118,11 @@ const Region: React.FC<TypeRegion> = ({ searchValue }) => {
             ))}
           </Select>
         </div>
+
+
       )}
+
+
     </div>
   );
 };
