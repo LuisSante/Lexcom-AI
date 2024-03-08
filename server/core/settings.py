786687946b5fe25ff6c225f0e087d30b5b26e355
LuisSante <lsantet@unsa.edu.pe>
2024-03-08 11:19:54 -0500

    fix: emails of recover password

--- server/core/settings.py
+++ server/core/settings.py
@@ -20,6 +20,7 @@ load_dotenv()
 
 DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
 PASSWORD_GMAIL = os.getenv('PASSWORD_GMAIL')
+PASSWORD_APP = os.getenv('PASSWORD_APP')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -191,12 +192,12 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_REFRESH_LIFETIME": timedelta(days=1),
 }
 
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = "lexcomsoporte@gmail.com"
-EMAIL_HOST_PASSWORD = f"{PASSWORD_GMAIL}"
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST_PASSWORD = f"{PASSWORD_APP}"
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
