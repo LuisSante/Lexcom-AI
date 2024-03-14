5b267d7d4977ced0861b0dbae6c02ffe30dbb42e
LuisSante <lsantet@unsa.edu.pe>
2024-03-14 18:35:09 -0500

    version : v1

--- server/core/settings.py
+++ server/core/settings.py
@@ -196,7 +196,6 @@ SIMPLE_JWT = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
