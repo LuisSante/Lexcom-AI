a23bdf39d95fbf0967a44556c596b97bcdd575ea
LuisSante <lsantet@unsa.edu.pe>
2024-04-10 21:25:46 -0500

    fix: refact env for production

--- client/src/components/logic/components_form/password_strong.ts
+++ client/src/components/logic/components_form/password_strong.ts
@@ -1,1 +1,3 @@
-export const strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/; // Enhanced regex
+// export const strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/; // Enhanced regex
+// export const strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.])[A-Za-z\d@$!%*?&.]{8,}$/;
+export const strongPasswordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&./(){}[\]~:-¨])[A-Za-z\d@$!%*?&./(){}[\]~:-¨]{8,}$/;
