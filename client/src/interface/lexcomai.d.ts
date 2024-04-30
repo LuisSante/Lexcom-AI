e0da92004d2fcf4ce7d61c44e0ea0afe4e9b6ead
LuisSante <lsantet@unsa.edu.pe>
2024-04-30 14:07:28 -0500

    feat: add new IA

--- client/src/interface/lexcomai.d.ts
+++ client/src/interface/lexcomai.d.ts
@@ -1,54 +1,58 @@
 export interface QuestionType {
-    id: string,
-    question: string,
+  id: string;
+  question: string;
 }
 
 export interface attibute_bool {
-    usabilidad: boolean;
-    innovacion: boolean;
-    diversificacion: boolean;
-    conformidad: boolean;
-    necesidad: boolean;
-    pasion: boolean;
-    calidad: boolean;
-    portabilidad: boolean;
-    complementariedad: boolean;
-    reconocimiento: boolean;
-    disponibilidad: boolean;
-    competencia: boolean;
-    frecuencia: boolean;
-    perdurabilidad: boolean;
-    innovacion_tecnica: boolean;
-    valoraciones: boolean;
-    realismo: boolean;
-    internacionalizacion: boolean;
-    estacionalidad: boolean;
-    abastecimiento: boolean;
-    percepcion_valor: boolean;
-    tamano: boolean;
-    frecuencia_estrategica: boolean;
-    agrupamiento: boolean;
-    rentabilidad: boolean;
-    analisis_ventas: boolean;
-    gastos_fijos: boolean;
-    publicidad: boolean;
+  persepcion_visual: boolean;
+  persepcion_unisex: boolean;
+  relevancia: boolean;
+  usabilidad: boolean;
+  innovacion: boolean;
+  diversificacion: boolean;
+  conformidad: boolean;
+  necesidad: boolean;
+  pasion: boolean;
+  calidad: boolean;
+  portabilidad: boolean;
+  complementariedad: boolean;
+  ads_library: boolean;
+  disponibilidad: boolean;
+  competencia: boolean;
+  frecuencia: boolean;
+  perdurabilidad: boolean;
+  innovacion_tecnica: boolean;
+  valoraciones: boolean;
+  realismo: boolean;
+  internacionalizacion: boolean;
+  estacionalidad: boolean;
+  variantes: boolean;
+  unisex: boolean;
+  emociones: boolean;
+  tamano: boolean;
+  frecuencia_estrategica: boolean;
+  agrupamiento: boolean;
+  utilidad: boolean;
+  rentabilidad: boolean;
+  analisis_ventas: boolean;
+  gastos_fijos: boolean;
+  publicidad: boolean;
 }
 
 export interface FormValues {
-    relevancia: number;
-    check: attibute_bool;
+  check: attibute_bool;
 }
 
 export interface TypePrediction {
-    labels: string[];
-    datasets: {
-        data: number[];
-        backgroundColor: string[];
-        borderColor: string[];
-        borderWidth: number;
-    }[];
+  labels: string[];
+  datasets: {
+    data: number[];
+    backgroundColor: string[];
+    borderColor: string[];
+    borderWidth: number;
+  }[];
 }
 
 export interface ChildrenProps {
-    children: ReactNode;
+  children: ReactNode;
 }
--- server/api/views/lexcomia.py
+++ server/api/views/lexcomia.py
@@ -2,25 +2,41 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from api.BackendClient.lexcomia import LexComIA_Client
-from api.serializers.lexcomai import LexcomSerializer
+from api.serializers.lexcomai import LexcomFiveClassSerializer, LexcomBinaryClassSerializer
+from rest_framework import viewsets
 
-class LexcomIA_ApiView(APIView):
+
+# class LexcomIA_ApiView(APIView):
+class LexcomIA_ApiView(viewsets.ViewSet):
     permission_classes = (IsAuthenticated, )
 
     def get_lexcom_client(self):
         client = LexComIA_Client()
         return client
 
-    def post(self, request):
-
+    def probability_percentage(self, request):
         data = request.data
         lexcom_client = self.get_lexcom_client()
-        response = lexcom_client.get_prediction(data)
-        serializer = LexcomSerializer(data={'very_good': response[0][0],
-                                            'good': response[0][1],
-                                            'normal':response[0][2],
-                                            'bad': response[0][3],
-                                            'very_bad':response[0][4]})
+        response = lexcom_client.get_prediction_five_class(data)
+        serializer = LexcomFiveClassSerializer(data={'very_good': response[0][0],
+                                                     'good': response[0][1],
+                                                     'normal': response[0][2],
+                                                     'bad': response[0][3],
+                                                     'very_bad': response[0][4]})
+        if serializer.is_valid():
+            serialized_data = serializer.data
+            # print(serialized_data)
+            return Response(serialized_data)
+        else:
+            return Response(serializer.errors, status=400)
+
+    def probability_success(self, request):
+        data = request.data
+        lexcom_client = self.get_lexcom_client()
+        response = lexcom_client.get_prediction_binary_class(data)
+        serializer = LexcomBinaryClassSerializer(
+            data={'success': response[0][0]})
+
         if serializer.is_valid():
             serialized_data = serializer.data
             # print(serialized_data)
