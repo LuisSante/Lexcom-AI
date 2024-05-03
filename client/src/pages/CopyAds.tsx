0854c5b8a5d5ee67d0a740684c413f8633e39d09
LuisSante <lsantet@unsa.edu.pe>
2024-05-02 19:12:47 -0500

    fix: change request form in copy module

--- client/src/pages/CopyAds.tsx
+++ client/src/pages/CopyAds.tsx
@@ -1,13 +1,31 @@
 import React, { useEffect, useState } from 'react'
 import './../css/TimelineDemo.css';
-import Skeleton from '../components/Skeleton';
 import axiosInstance from '../components/axios';
-import { notification } from 'antd';
+import { Skeleton, notification } from 'antd';
 import { CopyAds, TypeCopyAds } from '../interface/copyads';
+
+// const Loading = () => {
+//     return (
+//         <>
+//             <div className="tutorial">
+//                 <Skeleton />
+//             </div>
+//             <div className="tutorial">
+//                 <Skeleton />
+//             </div>
+//             <div className="tutorial">
+//                 <Skeleton />
+//             </div>
+//             <div className="tutorial">
+//                 <Skeleton />
+//             </div>
+//         </>
+//     )
+// }
 
 const CopyAI: React.FC<TypeCopyAds> = ({ searchValue }) => {
 
-    const [data, setData] = useState<CopyAds | null>(null);
+    const [data, setData] = useState<CopyAds[]>([]);
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [api, contextHolder] = notification.useNotification();
 
@@ -15,9 +33,10 @@ const CopyAI: React.FC<TypeCopyAds> = ({ searchValue }) => {
         const fetchData = async () => {
             setIsLoading(true);
             try {
-                const url = `copy_ads/${searchValue}`;
-                const response = await axiosInstance.get<CopyAds>(url);
-                setData(response.data);
+                const urls = Array.from({ length: 4 }, () => `copy_ads/${searchValue}`);
+                const responses = await Promise.all(urls.map(url => axiosInstance.get<CopyAds>(url)));
+                const allData = responses.map(response => response.data);
+                setData(allData);
                 api.success({
                     message: 'Prompt generado',
                     duration: 4
@@ -31,9 +50,9 @@ const CopyAI: React.FC<TypeCopyAds> = ({ searchValue }) => {
                 setIsLoading(false);
             }
         };
-    
+
         fetchData();
-    
+
         // Limpieza, en este caso no es necesario, pero si se agregan dependencias, deberían ir aquí
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
@@ -42,21 +61,38 @@ const CopyAI: React.FC<TypeCopyAds> = ({ searchValue }) => {
         <>
             {contextHolder}
             <div>
-                {isLoading && <Skeleton />}
-                {data && !isLoading && (
-                    <div className='tutorial'>
-                        {data.prompt.split('<br>').map((paragraph, pIndex) => (
-                            <React.Fragment key={`paragraph-${pIndex}`}>
+                {isLoading &&
+                    <>
+                        <div className="tutorial">
+                            <Skeleton active />
+                        </div>
+                        <div className="tutorial">
+                            <Skeleton active />
+                        </div>
+                        <div className="tutorial">
+                            <Skeleton active />
+                        </div>
+                        <div className="tutorial">
+                            <Skeleton active />
+                        </div>
+                    </>
+                }
+                {data.map((promptData, index) => (
+                    <div className='tutorial' key={index}>
+                        <span className='text-[#1677ff] text-lg'> LexCopy Pro {index + 1}</span>
+                        <br /><br />
+                        {promptData.prompt.split('<br>').map((paragraph, pIndex) => (
+                            <React.Fragment key={`paragraph-${index}-${pIndex}`}>
                                 {paragraph.split('\u2705').map((line, lineIndex) => (
-                                    <React.Fragment key={`line-${pIndex}-${lineIndex}`}>
+                                    <React.Fragment key={`line-${index}-${pIndex}-${lineIndex}`}>
                                         {lineIndex !== 0 && '\u2705'}{line}{(lineIndex < paragraph.split('\u2705').length - 1) && <br />}
                                     </React.Fragment>
                                 ))}
-                                {pIndex < data.prompt.split('<br>').length - 1 && <br />}
+                                {pIndex < promptData.prompt.split('<br>').length - 1 && <br />}
                             </React.Fragment>
                         ))}
                     </div>
-                )}
+                ))}
             </div>
         </>
     );
