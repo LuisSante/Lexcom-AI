64976e8c25f3fcfe55c74565ad88d50b9dbd2e07
LuisSante <lsantet@unsa.edu.pe>
2024-05-05 18:59:46 -0500

    fix: change lexcom guide

--- client/src/pages/Tutorial.tsx
+++ client/src/pages/Tutorial.tsx
@@ -3,17 +3,10 @@ import './../css/TimelineDemo.css';
 
 const Tutorial: React.FC = () => {
 
-  const content = (
-    <div>
-      <p>Al explorar el mapamundi, anota el porcentaje de interés</p>
-      <p> del país en el que venderás tu producto. </p>
-    </div>
-  )
-
   const content1 = (
     <div>
       <p>La primera calculadora te ofrecerá cosas estandares</p>
-      <p> Además podras calcular el cpa y cvu y el precio de tus productos</p>
+      <p> Además podras calcular el CPA y CVU y el precio de tus productos</p>
     </div>
   )
 
@@ -30,18 +23,18 @@ const Tutorial: React.FC = () => {
       <p>Recuerda hacer búsquedas en el idioma</p>
       <p> de tu público objetivo</p>
     </div>
-  )  
+  )
   const content4 = (
     <div>
       <p>Cada estructura  generada puede variar</p>
       <p> por lo que te sugerimos guardarlos</p>
     </div>
-  )  
+  )
   const content5 = (
     <div>
       <p>Tenemos cupones y descuentos disponibles</p>
     </div>
-  )  
+  )
   return (
     <div className='tutorial'>
       <ConfigProvider
@@ -55,15 +48,15 @@ const Tutorial: React.FC = () => {
         }}
       >
         <Timeline
-          mode={'alternate'}
           items={[
             {
               color: 'green',
               children: (
                 <>
-                  <Card title="GeoTrend Lex" bordered={false} style={{ width: '100%' }}>
-                    <p>Explora la pestaña de ranking de ventas en la parte izquierda de tu pantalla. Con esta pestaña podrás conocer si tu producto tiene un buen interés y en que países es el más buscado.</p>
-                    <Popover content={content}>
+                  <Card title="AutoPro Finance" bordered={false}>
+                    <Descriptions.Item label="UserName">Te ofrecemos tres calculadoras para que de esta forma puedas saber que precio debe tener tu producto, el CPA y CPU asi como estadísticas generales.</Descriptions.Item>
+                    <p></p>
+                    <Popover content={content1}>
                       <Button type="dashed" danger>¡Vamos!</Button>
                     </Popover>
                   </Card>
@@ -71,26 +64,14 @@ const Tutorial: React.FC = () => {
               ),
             },
             {
+              color: 'purple',
               children: (
                 <>
-                  <Card title="Budget Control Pro" bordered={false} style={{ width: '100%' }}>
-                    <Descriptions.Item label="UserName">Te ofrecemos tres calculadoras para que de esta forma puedas saber que precio debe tener tu producto, el CPA y CPU asi como estadisticas generales.</Descriptions.Item>
-                    <p></p>
-                    <Popover content={content1}>
-                      <Button type="dashed"danger>¡Vamos!</Button>
-                    </Popover>
-                  </Card>
-                </>
-              ),
-            },
-            {
-              children: (
-                <>
-                  <Card title="LexIA Determination" bordered={false} style={{ width: '100%' }}>
+                  <Card title="LexIA Determination" bordered={false}>
                     <Descriptions.Item label="UserName">Aquí conocerás si tu producto tendrá éxito o no a la hora de vender. Completa el formulario y no olvides poner el interés que anotaste anteriormente.</Descriptions.Item>
                     <p></p>
                     <Popover content={content2}>
-                      <Button type="dashed"danger>¡Vamos!</Button>
+                      <Button type="dashed" danger>¡Vamos!</Button>
                     </Popover>
                   </Card>
                 </>
@@ -100,7 +81,7 @@ const Tutorial: React.FC = () => {
               color: 'blue',
               children: (
                 <>
-                  <Card title="TikTok TrendFeed" bordered={false} style={{ width: '100%' }}>
+                  <Card title="TikTok TrendFeed" bordered={false}>
                     <Descriptions.Item label="UserName">Visualizar a la competencia es clave del éxito, ya que podrás conocer sus puntos débiles y usarlo a tu favor.
                       Con esta ventana tendrás los tiktoks más populares de tu producto, úsalos de inspiración para elaborar tu propio video ganador.</Descriptions.Item>
                     <p></p>
@@ -115,9 +96,9 @@ const Tutorial: React.FC = () => {
               color: 'red',
               children: (
                 <>
-                  <Card title="Prompt Generators" bordered={false} style={{ width: '100%' }}>
+                  <Card title="LexGeneration / LexVid Pro" bordered={false}>
                     <Descriptions.Item label="UserName">¿Tu producto es ganador, pero no sabes como estructurar tu video para que sea una venta segura?
-                      En esta pestaña obtendrás ideas de como hacerlo, te brindamos una estructura ganadora para que puedas estructurar tu video y generar tus copys.</Descriptions.Item>
+                      En esta módulo obtendrás ideas de como hacerlo, te brindamos una estructura ganadora para que puedas estructurar tu video. </Descriptions.Item>
                     <p></p>
                     <Popover content={content4}>
                       <Button type="dashed" danger>¡Vamos!</Button>
@@ -127,10 +108,38 @@ const Tutorial: React.FC = () => {
               ),
             },
             {
-              color: 'red',
+              color: 'lime-10',
               children: (
                 <>
-                  <Card title="Lexcom Courses" bordered={false} style={{ width: '100%' }}>
+                  <Card title="LexGeneration / LexLanding Pro" bordered={false}>
+                    <Descriptions.Item label="UserName">Encanta a tu audiencia con mensajes que cautivan. Nuestro servicio de copywriting te ayuda a destacar entre la multitud, comunicando de manera efectiva el valor de tu producto o servicio. Haz que tus palabras vendan por ti.</Descriptions.Item>
+                    <p></p>
+                    <Popover content={content4}>
+                      <Button type="dashed" danger>¡Vamos!</Button>
+                    </Popover>
+                  </Card>
+                </>
+              ),
+            },
+            {
+              color: 'magenta',
+              children: (
+                <>
+                  <Card title="LexGeneration / LexVid Pro" bordered={false}>
+                    <Descriptions.Item label="UserName">¿Quieres que tus páginas web atraigan y conviertan a tus visitantes? Descubre cómo este módulo te brinda una estructura para tus landing pages..</Descriptions.Item>
+                    <p></p>
+                    <Popover content={content4}>
+                      <Button type="dashed" danger>¡Vamos!</Button>
+                    </Popover>
+                  </Card>
+                </>
+              ),
+            },
+            {
+              color: 'indigo',
+              children: (
+                <>
+                  <Card title="Lexcom Courses" bordered={false}>
                     <Descriptions.Item label="UserName">¿No sabes como continuar vendiendo? Te ofrecemos una amplia variedad de cursos que te permitirán continuar tu camino al éxito.</Descriptions.Item>
                     <p></p>
                     <Popover content={content5}>
@@ -140,7 +149,7 @@ const Tutorial: React.FC = () => {
                 </>
               ),
             },
-            
+
           ]}
         />
       </ConfigProvider>
